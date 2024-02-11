c108c01c4261f5267ef811dfa33439a9cd9bfec0
Christian <chrisv1180-github@yahoo.com>
2024-02-11 17:13:54 +0100

    loop skeleton

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import logging
 import argparse
+import time
 import ShellyPy
 from shelly2sgready.config import load_config
 from shelly2sgready.sgready import SGReadyControl, SGReadyStates
+
+from waermepumpensteuerung.controller_loop import ControllerLoop
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -30,11 +34,29 @@ def main():
     # status = shelly_plug.relay(conf.shelly_state_2_3_relay)
     # print(status["output"])
 
-    waermepumpe = SGReadyControl(path_to_config=args.cfg_path)
-    #waermepumpe.setState(SGReadyStates.state2_normal)
-    print(waermepumpe.shelly_state_2_3.relay(0))
-    print(waermepumpe.shelly_optional.relay(0))
-    print(waermepumpe.getState())
+    # waermepumpe = SGReadyControl(path_to_config=args.cfg_path)
+    # #waermepumpe.setState(SGReadyStates.state2_normal)
+    # print(waermepumpe.shelly_state_2_3.relay(0))
+    # print(waermepumpe.shelly_optional.relay(0))
+    # print(waermepumpe.getState())
+
+    while True:
+        # Loop forever so that if an exception occurs, controller will restart
+        try:
+            waermepumpe = SGReadyControl(path_to_config=args.cfg_path)
+            logging.info(waermepumpe.shelly_state_2_3.relay(0))
+            logging.info(waermepumpe.shelly_optional.relay(0))
+            logging.info(waermepumpe.getState())
+
+            S = ControllerLoop(conf, logging)
+
+            S.run()
+        except Exception as e:
+            logging.error("%s: %s", str(type(e)), e)
+
+            # sleep 5 minutes to recover
+            time.sleep(300)
+            logging.info("Restarting waermepumpensteuerung")
 
 
 if __name__ == '__main__':
---
+++ waermepumpensteuerung/controller_loop.py
@@ -0,0 +1,41 @@
+from datetime import datetime, date
+import time
+
+from influxdb_client import WritePrecision, InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+from shelly2sgready.config import Config
+
+class ControllerLoop:
+
+    def __init__(self, cfg: Config, logging) -> None:
+        self.logging = logging
+        self.cfg = cfg
+
+        self.interval = 60  # seconds
+
+        # influxdb_client = InfluxDBClient(
+        #     url=cfg.influxdb_url,
+        #     token=cfg.influxdb_token,
+        #     org=cfg.influxdb_org
+        # )
+        # self.influxdb_write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
+        # self.influxdb_query_api = influxdb_client.query_api()
+        #
+        # # Used to track the transition to the next day for daily measurements
+        # self.todays_date = date.today()
+
+
+    def run(self):
+        error_count = 0
+        while True:
+            try:
+                self.logging.info("loop start")
+                time.sleep(self.interval)
+            except :
+                error_count += 1
+                self.logging.warning("Error (%d/10)", error_count)
+                if error_count >= 10:
+                    # Give up after a while
+                    raise
+                pass
